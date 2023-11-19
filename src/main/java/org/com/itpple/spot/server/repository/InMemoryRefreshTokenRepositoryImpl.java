4e64392df3726e8fbd7136034c146e6d9d120247
9pruruluru2 <9pruruluru2@naver.com>
2023-11-19 23:32:20 +0900

    #1 [feat] RefreshToken을 저장할 임시 repository 구현

---
+++ src/main/java/org/com/itpple/spot/server/repository/InMemoryRefreshTokenRepositoryImpl.java
@@ -0,0 +1,27 @@
+package org.com.itpple.spot.server.repository;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class InMemoryRefreshTokenRepositoryImpl implements RefreshTokenRepository {
+
+  private final Map<Long, String> refreshTokenMap = new HashMap<>();
+
+  @Override
+  public void save(Long userId, String refreshToken) {
+    refreshTokenMap.put(userId, refreshToken);
+  }
+
+  @Override
+  public Optional<String> findRefreshTokenByUserId(Long userId) {
+    return Optional.empty();
+  }
+
+  @Override
+  public void removeByUserId(Long userId) {
+
+  }
+}
