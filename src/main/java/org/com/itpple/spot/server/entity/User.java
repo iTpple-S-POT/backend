02bd497e99aa5475fb1db2bf95ed956a51d8e577
9pruruluru2 <9pruruluru2@naver.com>
2023-11-20 22:14:45 +0900

    #3 [feat] userEntity 좀 더 의미있게 수정

--- src/main/java/org/com/itpple/spot/server/entity/User.java
+++ src/main/java/org/com/itpple/spot/server/entity/User.java
@@ -2,6 +2,8 @@ package org.com.itpple.spot.server.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import lombok.AllArgsConstructor;
@@ -9,6 +11,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.com.itpple.spot.server.model.Role;
 
 @Slf4j
 @Entity(name = "sp_user")
@@ -16,7 +19,7 @@ import lombok.extern.slf4j.Slf4j;
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
-public class User {
+public class User extends BasicDateEntity {
 
   @Id
   @GeneratedValue
@@ -24,4 +27,14 @@ public class User {
 
   @Column(name = "o_auth_id", nullable = false)
   private String oAuthId;
+
+  @Enumerated(EnumType.STRING)
+  @Column(name = "role", nullable = false)
+  private Role role;
+
+  @Column(name = "nickname")
+  private String nickname;
+
+  @Column(name = "profile_image_url")
+  private String profileImageUrl;
 }
---
+++ src/main/java/org/com/itpple/spot/server/service/UserService.java
@@ -0,0 +1,12 @@
+package org.com.itpple.spot.server.service;
+
+import java.util.Optional;
+import org.com.itpple.spot.server.entity.User;
+
+public interface UserService {
+  User save(User user);
+
+  Optional<User> findByoAuthId(String oAuthId);
+
+  Optional<User> findById(Long id);
+}
