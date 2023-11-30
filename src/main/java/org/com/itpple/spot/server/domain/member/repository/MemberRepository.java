a5d204188952c53c77064cf40b99819b44f953c4
Somin Yun <somin455@naver.com>
2023-11-30 22:38:51 +0900

    remove: 기존에 있던 User에 합침

--- src/main/java/org/com/itpple/spot/server/domain/member/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package org.com.itpple.spot.server.domain.member.repository;
-
-import org.com.itpple.spot.server.domain.member.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    boolean existsByNickname(String nickname);
-}
