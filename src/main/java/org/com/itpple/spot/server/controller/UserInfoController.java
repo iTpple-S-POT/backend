1f3aa80173ab3007a37f423de5bae2b82e7b8d9f
Somin Yun <somin455@naver.com>
2023-11-30 23:32:22 +0900

    fix: 닉네임중복체크 수정

--- src/main/java/org/com/itpple/spot/server/controller/UserInfoController.java
+++ src/main/java/org/com/itpple/spot/server/controller/UserInfoController.java
@@ -3,7 +3,6 @@ package org.com.itpple.spot.server.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.com.itpple.spot.server.model.dto.userInfo.UserRequestDto;
-import org.com.itpple.spot.server.model.dto.userInfo.NicknameRequestDto;
 import org.com.itpple.spot.server.service.userInfo.UserInfoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,7 +33,7 @@ public class UserInfoController {
 
     //닉네임 중복 체크
     @GetMapping("/fill-basic-userinfo/check-duplicate-nickname")
-    public ResponseEntity<Void> checkDuplicateNickname(@RequestParam("nickname") @Valid String nickname) {
+    public ResponseEntity<Void> checkDuplicateNickname(@RequestParam("nickname") String nickname) {
         try {
             if (userInfoService.isAlreadyExistNickname(nickname)) {
                 throw new Exception("중복된 닉네임입니다");
@@ -42,7 +41,7 @@ public class UserInfoController {
             return ResponseEntity.ok().build();
         } catch (Exception e) {
             log.error(e.getMessage());    //향후 custom 에러로 처리
-            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
 
