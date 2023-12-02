e2c583f4c45f0e22151bb065f55d0bbc122c9711
sujeong11 <qalzm7351@gmail.com>
2023-12-02 13:47:42 +0900

    feat: 테스트 메서드 추가

--- src/main/java/org/com/itpple/spot/server/config/SecurityConfig.java
+++ src/main/java/org/com/itpple/spot/server/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
+                .antMatchers("/test").permitAll() // TODO: 프론트와 연결 확인 후 제거
                 .antMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
                 .antMatchers("/auth/login/**").permitAll()
                 .antMatchers("/auth/refresh").permitAll()
---
+++ src/main/java/org/com/itpple/spot/server/controller/TestController.java
@@ -0,0 +1,18 @@
+package org.com.itpple.spot.server.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// TODO: 프론트와 연결 확인 후 제거
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity test() {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body("test 확인");
+    }
+}
