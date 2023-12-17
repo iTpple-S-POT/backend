7d6f06abfd1391f16a1beecdca402b67124f37d0
9pruruluru2 <9pruruluru2@naver.com>
2023-12-17 21:34:38 +0900

    #31 fix: test 시 @Value가 제대로 넣어지지 않아 생성자를 통해 입력할 수 있도록 수정

--- src/test/java/org/com/itpple/spot/server/core/TokenProviderTest.java
+++ src/test/java/org/com/itpple/spot/server/core/TokenProviderTest.java
@@ -1,91 +1,85 @@
-//package org.com.itpple.spot.server.core;
-//
-//
-//import static org.junit.jupiter.api.Assertions.assertAll;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertThrows;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//import java.util.HashSet;
-//import org.com.itpple.spot.server.common.jwt.TokenProvider;
-//import org.junit.jupiter.api.Test;
-//import org.junit.jupiter.api.extension.ExtendWith;
-//import org.mockito.InjectMocks;
-//import org.mockito.junit.jupiter.MockitoExtension;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.GrantedAuthority;
-//
-//@ExtendWith(MockitoExtension.class)
-//public class TokenProviderTest {
-//
-//    @InjectMocks
-//    private TokenProvider generateTokenService;
-//
-//    @Test
-//    public void testIssueAccessToken() {
-//        var id = 1L;
-//        var role = "ROLE_USER";
-//
-//        //role to authentication
-//        var authorities = new HashSet<GrantedAuthority>();
-//        authorities.add((GrantedAuthority) () -> role);
-//
-//        var authentication = new UsernamePasswordAuthenticationToken(id, null, authorities);
-//
-//        var generatedToken = generateTokenService.generateToken(id, authentication);
-//        var payload = generateTokenService.getPayload(generatedToken.getAccessToken());
-//
-//        assertAll(() -> assertEquals(payload.getId(), "testId"),
-//                () -> assertTrue(payload.getExpiration().getTime() > System.currentTimeMillis()),
-//                () -> assertTrue(payload.getIssuedAt().getTime() <= System.currentTimeMillis())
-//        );
-//    }
-//
-//    @Test
-//    public void testIssueRefreshToken() {
-//        var id = 1L;
-//
-//        var generatedToken = generateTokenService.generateToken(id, null);
-//        var payload = generateTokenService.getPayload(generatedToken.getRefreshToken());
-//
-//        assertAll(() -> assertEquals(payload.getId(), "testId"),
-//                () -> assertTrue(payload.getExpiration().getTime() > System.currentTimeMillis()),
-//                () -> assertTrue(payload.getIssuedAt().getTime() <= System.currentTimeMillis())
-//        );
-//    }
-//
-//    @Test
-//    public void TestExceptionAccessTokenIsModified() {
-//        var id = 1L;
-//        var role = "ROLE_USER";
-//        var authorities = new HashSet<GrantedAuthority>();
-//        authorities.add((GrantedAuthority) () -> role);
-//
-//        var authentication = new UsernamePasswordAuthenticationToken(id, null, authorities);
-//
-//        var accessToken =
-//                generateTokenService.generateToken(id, authentication).getAccessToken() + "a";
-//
-//        assertThrows(RuntimeException.class, () -> generateTokenService.getPayload(accessToken));
-//    }
-//
-//    @Test
-//    public void TestExceptionAccessTokenIsExpired() {
-//        var id = 1L;
-//        var role = "ROLE_USER";
-//        var authorities = new HashSet<GrantedAuthority>();
-//        authorities.add((GrantedAuthority) () -> role);
-//
-//        var authentication = new UsernamePasswordAuthenticationToken(id, null, authorities);
-//
-//        var accessToken = generateTokenService.generateToken(id, authentication).getAccessToken();
-//
-//        try {
-//            Thread.sleep(1000L);
-//        } catch (InterruptedException e) {
-//            e.printStackTrace();
-//        }
-//
-//        assertThrows(RuntimeException.class, () -> generateTokenService.getPayload(accessToken));
-//    }
-//}
+package org.com.itpple.spot.server.core;
+
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.HashSet;
+import org.com.itpple.spot.server.common.jwt.TokenProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+@ExtendWith({MockitoExtension.class})
+public class TokenProviderTest {
+
+    @InjectMocks
+    private TokenProvider tokenProvider;
+
+    @BeforeEach
+    public void setUp() {
+        tokenProvider = new TokenProvider(
+                "testKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKey",
+                1L,
+                "testKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKeytestKey",
+                1L);
+    }
+
+    @Test
+    public void testIssueAccessToken() {
+        var id = 1L;
+        var role = "ROLE_USER";
+        //role to authentication
+        var authorities = new HashSet<GrantedAuthority>();
+        authorities.add((GrantedAuthority) () -> role);
+
+        var authentication = new UsernamePasswordAuthenticationToken(id, null, authorities);
+
+        var generatedToken = tokenProvider.generateToken(id, authentication);
+        var payload = tokenProvider.getPayload(generatedToken.getAccessToken());
+
+        assertAll(() -> assertEquals("1", payload.getSubject()),
+                () -> assertTrue(payload.getExpiration().getTime() > System.currentTimeMillis()),
+                () -> assertTrue(payload.getIssuedAt().getTime() <= System.currentTimeMillis()));
+    }
+
+    @Test
+    public void TestExceptionAccessTokenIsModified() {
+        var id = 1L;
+        var role = "ROLE_USER";
+        var authorities = new HashSet<GrantedAuthority>();
+        authorities.add((GrantedAuthority) () -> role);
+
+        var authentication = new UsernamePasswordAuthenticationToken(id, null, authorities);
+
+        var accessToken = tokenProvider.generateToken(id, authentication).getAccessToken() + "a";
+
+        assertThrows(RuntimeException.class, () -> tokenProvider.getPayload(accessToken));
+    }
+
+    @Test
+    public void TestExceptionAccessTokenIsExpired() {
+        var id = 1L;
+        var role = "ROLE_USER";
+        var authorities = new HashSet<GrantedAuthority>();
+        authorities.add((GrantedAuthority) () -> role);
+
+        var authentication = new UsernamePasswordAuthenticationToken(id, null, authorities);
+
+        var accessToken = tokenProvider.generateToken(id, authentication).getAccessToken();
+
+        try {
+            Thread.sleep(2000L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        assertThrows(RuntimeException.class, () -> tokenProvider.getPayload(accessToken));
+    }
+}
