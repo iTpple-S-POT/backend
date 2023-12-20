ade1f9c537cdc8d69e807a8b121fb232b3c8d772
Somin Yun <somin455@naver.com>
2023-12-20 12:09:14 +0900

    #14 feat: user exception

--- src/main/java/org/com/itpple/spot/server/exception/code/ErrorCode.java
+++ src/main/java/org/com/itpple/spot/server/exception/code/ErrorCode.java
@@ -14,8 +14,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ErrorCode {
 
-    ;
-
+    MEMBER_ID_ALREADY_EXISTS(400,"이미 존재하는 meberId 입니다", HttpStatus.BAD_REQUEST),
+    NICKNAME_DUPLICATE(500,"이미 존재하는 닉네임 입니다",HttpStatus.INTERNAL_SERVER_ERROR),
+    NICKNAME_VALIDATION(400,"공백없이 15자 이하로 작성해주세요 특수문자는 _만 사용 가능해요",HttpStatus.BAD_REQUEST);
     private final Integer code;
     private final String message;
     private final HttpStatus httpStatus;
