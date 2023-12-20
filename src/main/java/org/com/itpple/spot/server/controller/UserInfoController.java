b1a523da807ec5eab1f54a6263896f73c1a81c94
Somin Yun <somin455@naver.com>
2023-12-20 12:11:18 +0900

    #14 fix: nickname valid 추가

--- src/main/java/org/com/itpple/spot/server/controller/UserInfoController.java
+++ src/main/java/org/com/itpple/spot/server/controller/UserInfoController.java
@@ -7,7 +7,6 @@ import org.com.itpple.spot.server.model.dto.userInfo.UserRequestDto;
 import org.com.itpple.spot.server.service.userInfo.UserInfoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 
 @Slf4j
@@ -30,18 +29,12 @@ public class UserInfoController {
         }
     }
 
-    //닉네임 중복 체크
+    //닉네임 체크
     @GetMapping("/nickname/check")
     public ResponseEntity<Void> nicknameCheck(@RequestParam("nickname") String nickname) {
-        try {
-            if (userInfoService.isAlreadyExistNickname(nickname)) {
-                throw new IllegalArgumentException("중복된 닉네임입니다");
-            }
-            return ResponseEntity.ok().build();
-        } catch (IllegalArgumentException e) {
-            log.error(e.getMessage());    //향후 custom 에러로 처리
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
-        }
+        userInfoService.validateNickname(nickname);
+        userInfoService.isAlreadyExistNickname(nickname);
+        return ResponseEntity.ok().build();
     }
 
 }
