f695bf84c1f64bf1bbb9f60297e9f418f35f08b4
9pruruluru2 <9pruruluru2@naver.com>
2023-12-23 00:16:01 +0900

    #22 fix: convertToPoint 좌표계를 위도/경도로 수정

--- src/main/java/org/com/itpple/spot/server/util/GeometryUtil.java
+++ src/main/java/org/com/itpple/spot/server/util/GeometryUtil.java
@@ -1,14 +1,31 @@
 package org.com.itpple.spot.server.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.PrecisionModel;
+import org.locationtech.jts.io.ParseException;
+import org.locationtech.jts.io.WKTReader;
 
+@Slf4j
 public class GeometryUtil {
+
     public static Point convertToPoint(Double lat, Double lon) {
-        GeometryFactory geometryFactory = new GeometryFactory();
+        GeometryFactory geometryFactory = new GeometryFactory(new PrecisionModel(), 4326);
         Coordinate coordinate = new Coordinate(lat, lon);
 
         return geometryFactory.createPoint(coordinate);
+    }
+
+    //wellKnownText is https://en.wikipedia.org/wiki/Well-known_text_representation_of_geometry
+    public static Point wktToGeometry(String wellKnownText) {
+        try {
+            var result =new WKTReader().read(wellKnownText);
+            return (Point) result;
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
