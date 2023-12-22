ddb4518e9a43a234718365fd8650bc1635e33c7d
9pruruluru2 <9pruruluru2@naver.com>
2023-12-23 01:39:29 +0900

    #22 rename: fileController로 역할 분리

--- src/main/java/org/com/itpple/spot/server/controller/PotController.java
+++ src/main/java/org/com/itpple/spot/server/controller/PotController.java
@@ -10,7 +10,6 @@ import org.com.itpple.spot.server.dto.pot.request.UploadImageRequest;
 import org.com.itpple.spot.server.dto.pot.response.CreatePotResponse;
 import org.com.itpple.spot.server.dto.pot.response.GetCategoryResponse;
 import org.com.itpple.spot.server.dto.pot.response.UploadImageResponse;
-import org.com.itpple.spot.server.service.FileService;
 import org.com.itpple.spot.server.service.PotService;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -28,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class PotController {
 
     private final PotService potService;
-    private final FileService fileService;
 
 
     @GetMapping(value = "/category", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -41,14 +38,8 @@ public class PotController {
             @Auth UserDetailsCustom userDetailsCustom,
             @Valid @RequestBody UploadImageRequest uploadImageRequest) {
         var userId = userDetailsCustom.getUserId();
+//        var userId = 1L;
         return ResponseEntity.ok(potService.uploadImage(userId, uploadImageRequest.fileName()));
-    }
-
-    //TODO: 프론트에서 S3 이미지 올라갔는지 확인하는 api(s3 테스트 후 삭제)
-    @GetMapping(value = "/image/check", produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> checkImage(
-            @RequestParam(name = "fileName") String fileName) {
-        return ResponseEntity.ok(fileService.isUploaded(fileName));
     }
 
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
