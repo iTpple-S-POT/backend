91b2aa87867c703baf1f61d9d1df0ed6c02a39d8
9pruruluru2 <9pruruluru2@naver.com>
2023-12-23 00:51:06 +0900

    #22 refactor: self-injection 제거

--- src/main/java/org/com/itpple/spot/server/common/auth/userDetails/UserDetailsCustomService.java
+++ src/main/java/org/com/itpple/spot/server/common/auth/userDetails/UserDetailsCustomService.java
@@ -1,11 +1,7 @@
 package org.com.itpple.spot.server.common.auth.userDetails;
 
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.com.itpple.spot.server.repository.UserRepository;
-import org.springframework.beans.BeansException;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,28 +9,21 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserDetailsCustomService implements UserDetailsService, ApplicationContextAware {
+public class UserDetailsCustomService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    private ApplicationContext context;
-
-    @Override
-    public void setApplicationContext(@NonNull ApplicationContext applicationContext)
-            throws BeansException {
-        this.context = applicationContext;
-    }
-
     public UserDetails loadUserByUserId(Long userId) throws UsernameNotFoundException {
-        return UserDetailsCustom.from(
-                userRepository.findById(userId)
-                        .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다.")));
+        return userRepository.findById(userId)
+                .map(UserDetailsCustom::from)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        var self = context.getBean(this.getClass());
-        return self.loadUserByUserId(Long.parseLong(username));
+        return userRepository.findById(Long.valueOf(username))
+                .map(UserDetailsCustom::from)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다."));
     }
 
 }
