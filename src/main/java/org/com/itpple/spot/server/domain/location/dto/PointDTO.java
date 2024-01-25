c2581f100b1afb3b0d4a9dae483c2f20fdca508b
9pruruluru2 <9pruruluru2@naver.com>
2024-01-25 23:39:45 +0900

    #73 fix: lat, lon 순서 잘못된 것 수정

--- src/main/java/org/com/itpple/spot/server/domain/location/dto/PointDTO.java
+++ src/main/java/org/com/itpple/spot/server/domain/location/dto/PointDTO.java
@@ -1,12 +1,13 @@
 package org.com.itpple.spot.server.domain.location.dto;
 
 import javax.validation.constraints.NotNull;
+import org.locationtech.jts.geom.Point;
 
 public record PointDTO(
         @NotNull Double lat,
         @NotNull Double lon) {
 
-    public static PointDTO from(org.locationtech.jts.geom.Point point) {
-        return new PointDTO(point.getY(), point.getX());
+    public static PointDTO from(Point point) {
+        return new PointDTO(point.getX(), point.getY());
     }
 }
